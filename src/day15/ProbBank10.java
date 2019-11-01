fa358148d06339e25f08e44e1d2d686ed5903bdd
sohee973 <shpark0201@naver.com>
2019-11-01 21:47:35 +0900

    Java

---
+++ src/day15/ProbBank10.java
@@ -0,0 +1,45 @@
+package day15;
+/*3^2+7x를 1~2까지 정적분하시오*/
+class Integral{
+    int secondTerm;
+    int firstTerm;
+    float delta;
+    float start;
+    float end;
+    float curX;
+    float allSum;
+    public Integral(float start,float end){
+        secondTerm=3;
+        firstTerm=7;
+        delta=0.001f;
+
+        this.start=start;
+        this.end=end;
+        curX=start;
+        allSum=0;
+    }
+    /*lim h->0 (f(x+h)-f(x)) / h
+    * f(x)=3x^2+7x
+    * f(x+h)=3(x+h)^2+7(x+h) */
+    public void calcIntegral(){
+        float tmp;
+        for(;curX<end;curX+=delta)
+        {
+            tmp=curX+delta;
+           allSum += delta*(secondTerm*(float)Math.pow(tmp,2.0)+firstTerm*tmp);
+        }
+
+    }
+    public String toString(){
+        return "3x^2 + 7x를 1~2까지 정적분한 결과 = " + allSum;
+    }
+}
+public class ProbBank10 {
+    public static void main(String[] args) {
+        Integral i= new Integral(1,2);
+        i.calcIntegral();
+
+        System.out.println(i);
+    }
+
+}
---
+++ src/day15/StaticTest.java
@@ -0,0 +1,29 @@
+package day15;
+
+class StaticCnt {
+    //static으로 선언한 변수는 data 영역에 배치된다.
+    // *** 즉, 전역 변수화가 됩니다.
+    static int cnt=0;
+
+    public StaticCnt(){
+        cnt++;
+        System.out.println("cnt = " +cnt);
+    }
+}
+public class StaticTest{
+    public static void test(){
+        System.out.println("test");
+    }
+    /* 함수가 전역 변수화 ???
+    * 함수는 TEXT 영역인데 ?
+    * 함수에 static이 붙는 것은 변수와는 동작이 다르다.
+    * 메모리 할당 전에 이미 로드가 완료 되어 있음 */
+    public static void main(String[] args){
+        StaticCnt sc1=new StaticCnt();
+        StaticCnt sc2=new StaticCnt();
+        StaticCnt sc3=new StaticCnt();
+
+        test();
+
+    }
+}
